37b6f33f163b17fb42e4811414e5e736c0ed11c4
Saddimanisha <saddimanishareddy@gmail.com>
2025-11-29 17:53:10 +0000

    python Libraries

--- Heart_failure.py
+++ Heart_failure.py
@@ -1,1 +1,17 @@
-print('hello')
+import kagglehub
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
+import warnings
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+import warnings
+warnings.filterwarnings("ignore")
